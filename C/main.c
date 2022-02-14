6b5b542a8a85180a941725d57f01f5a2945fadd3
tosh7 <zlia.6.lj.425@gmail.com>
2022-02-14 18:07:40 +0900

    update C program

--- C/main.c
+++ C/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    printf("hello world\n");
+int main(void);
+int culculator(int);
+
+int main(void) {
+    int inputNumber, outputNumber;
+    printf("Input a number then output a culculated number.\n");
+    scanf("%d", &inputNumber);
+    outputNumber = culculator(inputNumber);
+    printf("%d\n", outputNumber);
     return 0;
+}
+
+int culculator(int param) {
+    return param + 10;
 }
